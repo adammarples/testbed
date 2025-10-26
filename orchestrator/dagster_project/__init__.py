8da13de83fcf81dbc76bd3e039fd13cb1d3af042
Adam Marples <adam.marples@marshmallow.co>
2025-10-26 18:08:42 +0000

    add job and schedule

--- orchestrator/dagster_project/__init__.py
+++ orchestrator/dagster_project/__init__.py
@@ -1,9 +1,45 @@
-from dagster import Definitions
-from .assets import dbt_transforms, dbt_resource
+from pathlib import Path
+from dagster import AssetExecutionContext, Definitions, DefaultScheduleStatus, define_asset_job, ScheduleDefinition
+from dagster_dbt import DbtCliResource, dbt_assets, DbtProject, DagsterDbtTranslator
 
+
+DBT_PROJECT_DIR = Path(__file__).parent.parent / "dbt_project"
+DBT_PROFILES_DIR = DBT_PROJECT_DIR
+
+dbt_project = DbtProject(
+    project_dir=DBT_PROJECT_DIR.as_posix(),
+    profiles_dir=DBT_PROFILES_DIR.as_posix(),
+)
+
+dbt_cli = DbtCliResource(
+    project_dir=dbt_project.project_dir,
+    profiles_dir=dbt_project.profiles_dir,
+)
+
+
+@dbt_assets(
+    manifest=dbt_project.manifest_path,
+    dagster_dbt_translator=DagsterDbtTranslator(), # enables asset checks for dbt tests
+)
+def dbt_transforms(context: AssetExecutionContext, dbt: DbtCliResource):
+    """
+    dbt models for transforming source data into staging and metrics.
+    """
+    yield from dbt.cli(["build"], context=context).stream()
+
+
+dbt_job = define_asset_job(name="dbt_job")
+
+daily_dbt_schedule = ScheduleDefinition(
+    job=dbt_job,
+    cron_schedule="*/2 * * * *",
+    default_status=DefaultScheduleStatus.RUNNING,
+)
+
+# main entrypoint for dagster
 defs = Definitions(
     assets=[dbt_transforms],
-    resources={
-        "dbt": dbt_resource,
-    },
+    resources={"dbt": dbt_cli},  # "dbt" key here is significant, it maps to the dbt parameter in the asset function
+    jobs=[dbt_job],
+    schedules=[daily_dbt_schedule],
 )
